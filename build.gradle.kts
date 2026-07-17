2d7305145dd6f1e03adb691d0fd7af34cf0e757a
jonatasbrito <jonatasbrito.sousa@gmail.com>
2026-07-17 14:00:02 -0300

    build: configure kotlin cli application and test baseline

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.4.0"
+    application
 }
 
 group = "calculator"
@@ -19,4 +20,12 @@ kotlin {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+application {
+    mainClass = "calculator.MainKt"
+}
+
+tasks.named<JavaExec>("run") {
+    standardInput = System.`in`
 }
---
+++ src/main/kotlin/calculator/Main.kt
@@ -0,0 +1,5 @@
+package calculator
+
+fun main() {
+    println("RPN Calculator")
+}
